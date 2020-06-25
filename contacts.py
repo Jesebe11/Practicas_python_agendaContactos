14720936bf2b01a79a179c7c529deaa189d031c3
Jesebell Escobar <jesebellinformatica@gmail.com>
2020-06-25 14:14:42 -0400

    Practicas de ython, agenda de contactos

---
+++ contacts.py
@@ -0,0 +1,136 @@
+import csv
+
+
+class Contact:
+
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
+class ContactBook:
+    def __init__(self):
+        self.contacts = []
+
+    def add(self, name, phone, email):
+        contact = Contact(name, phone, email)
+        self.contacts.append(contact)
+        self._save()
+
+    def show_all(self):
+        for contact in self.contacts:
+            self._print_contact(contact)
+
+    def delete(self, name):
+        for idx, contact in enumerate(self.contacts):
+            if contact.name.lower() == name.lower():
+                del self.contacts[idx]
+                self._save()
+                break
+
+    def search(self, name):
+        for contact in self.contacts:
+            if contact.name.lower() == name.lower():
+                self._print_contact(contact)
+                break
+        else:
+            self._not_found()
+
+    def update(self, name):
+        for contact in self.contacts:
+            self._change_contact(contact)
+            self._save()
+            break
+
+    def _save(self):
+        with open('contacts.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow( ('name', 'phone', 'email') )
+
+            for contact in self.contacts:
+                writer.writerow( (contact.name, contact.phone, contact.email) )
+
+
+
+    def _print_contact(self, contact):
+        print('--- * --- * --- * --- * --- * ---')
+        print('Nombre: {}'.format(contact.name))
+        print('Teléfono: {}'.format(contact.phone))
+        print('Email: {}'.format(contact.email))
+        print('--- * --- * --- * --- * --- * ---')
+
+    def _not_found(self):
+        print('****')
+        print('¡No encontrado!')
+        print('****')
+
+    def _change_contact(self, contact):
+        contact.name = str(input('Escriba el nuevo nombre del contacto: '))
+        contact.phone = str(input('Escriba el nuevo número del contacto: '))
+        contact.email = str(input('Escriba el nuevo email del contacto: '))
+        print('Nuevos datos: nombre: {}, teléfono: {}, email: {}.'.format(contact.name, contact.phone, contact.email))
+
+def run():
+
+    contact_book = ContactBook()
+
+    with open('contacts.csv', 'r') as f:
+        reader = csv.reader(f)
+        for idx, row in enumerate(reader):
+            if idx == 0:
+                continue
+
+            contact_book.add(row[0], row[1], row[2])
+
+    while True:
+        command = str(input('''
+        ¿Qué deseas hacer?
+
+        [a]ñadir contacto
+        [ac]tualizar contacto
+        [b]uscar contacto
+        [e]liminar contacto
+        [l]istar contactos
+        [s]alir
+        
+        '''))
+
+        if command == 'a':
+            name = str(input('Escribe el nombre del contacto: '))
+            phone = str(input('Escribe el tel del contacto: '))
+            email = str(input('Escribe el email del contacto: '))
+
+            contact_book.add(name, phone, email)
+
+        elif command == 'ac':
+            name = str(input('Escribe el nombre del contacto a actualizar: '))
+
+            contact_book.update(name)
+
+        elif command == 'b':
+            name = str(input('Escribe el nombre del contacto: '))
+
+            contact_book.search(name)
+
+
+
+        elif command == 'e':
+            name = str(input('Escribe el nombre del contacto: '))
+
+            contact_book.delete(name)
+
+        elif command == 'l':
+            
+            contact_book.show_all()
+
+        elif command == 's':
+            break
+
+        else:
+            print('Comando no encontrado.')
+
+
+if __name__ == '__main__':
+    print('BIENVENIDO A LA AGENDA')
+    run()
